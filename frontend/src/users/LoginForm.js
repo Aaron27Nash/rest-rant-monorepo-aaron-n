7016b14324b6e6d2c978fbac3ef6967506102233
Aaron Nash <aaron27nash@gmail.com>
2023-10-23 22:34:48 -0400

    finished hashed password activity

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -17,6 +17,21 @@ function LoginForm() {
 
     async function handleSubmit(e) {
         e.preventDefault()
+        const respone = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        const data = await Response.json()
+        if (Response.staus===200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        
+        } else
+            setErrorMessage(data.message)
+    
        
 
     }
